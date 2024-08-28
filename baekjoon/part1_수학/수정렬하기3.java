5615c03b8d668ccb5853149df8e73c874ad4d411
ddi00 <ehfus963@gmail.com>
2024-08-28 17:50:16 +0900

    수 정렬하기 3

---
+++ baekjoon/part1_수학/수정렬하기3.java
@@ -0,0 +1,26 @@
+package part1_수학;
+
+import java.io.*;
+
+public class 수정렬하기3 {
+
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int[] arr = new int[10001];
+        int num = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<num; i++){
+            arr[Integer.parseInt(br.readLine())]++;
+        }
+
+        for(int i=0; i<arr.length; i++){
+            while(arr[i] > 0){
+                bw.write(i+"\n");
+                arr[i]--;
+            }
+        }
+        bw.flush();
+    }
+}
